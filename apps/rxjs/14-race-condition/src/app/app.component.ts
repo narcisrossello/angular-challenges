087f297d0df83366811169ddb76be45ab5ca6122
Narcis <narcisrossello@gmail.com>
2024-06-04 20:14:50 +0200

    feat: solve challenge 14

--- apps/rxjs/14-race-condition/src/app/app.component.ts
+++ apps/rxjs/14-race-condition/src/app/app.component.ts
@@ -1,11 +1,7 @@
-import {
-  ChangeDetectionStrategy,
-  Component,
-  inject,
-  OnInit,
-} from '@angular/core';
+import { AsyncPipe, NgIf } from '@angular/common';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
-import { take } from 'rxjs';
+import { Observable } from 'rxjs';
 import { TopicModalComponent } from './topic-dialog.component';
 import { TopicService, TopicType } from './topic.service';
 
@@ -13,27 +9,23 @@ import { TopicService, TopicType } from './topic.service';
   standalone: true,
   selector: 'app-root',
   template: `
-    <button (click)="openTopicModal()">Open Topic</button>
+    <button *ngIf="topics$ | async as topics" (click)="openTopicModal(topics)">
+      Open Topic
+    </button>
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [NgIf, AsyncPipe],
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'rxjs-race-condition';
   dialog = inject(MatDialog);
   topicService = inject(TopicService);
-  topics: TopicType[] = [];
+  topics$: Observable<TopicType[]> = this.topicService.fakeGetHttpTopic();
 
-  ngOnInit(): void {
-    this.topicService
-      .fakeGetHttpTopic()
-      .pipe(take(1))
-      .subscribe((topics) => (this.topics = topics));
-  }
-
-  openTopicModal() {
+  openTopicModal(topics: TopicType[]) {
     this.dialog.open(TopicModalComponent, {
       data: {
-        topics: this.topics,
+        topics: topics,
       },
     });
   }
