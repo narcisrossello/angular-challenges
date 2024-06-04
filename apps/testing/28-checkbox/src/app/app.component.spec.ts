79aac5c6240ee6502d990fe5390aba43859cd087
Narcis <narcisrossello@gmail.com>
2024-06-04 19:33:38 +0200

    feat: solve challenge 28

--- apps/testing/28-checkbox/src/app/app.component.spec.ts
+++ apps/testing/28-checkbox/src/app/app.component.spec.ts
@@ -1,10 +1,24 @@
-import { render } from '@testing-library/angular';
+import { render, screen } from '@testing-library/angular';
+import userEvent from '@testing-library/user-event';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  const setup = async () => {
+    const user = userEvent.setup();
+    const result = await render(AppComponent);
+    return { result, user };
+  };
   describe('When checking the checkbox', () => {
     it('Then button is enabled', async () => {
-      await render(AppComponent);
+      const { user } = await setup();
+
+      const checkboxLabel = await screen.findByText('Agreed');
+      await user.click(checkboxLabel);
+      const checkbox = await screen.findByRole('checkbox');
+      expect(checkbox).toBeChecked();
+
+      const button = await screen.findByRole('button', { name: 'Submit' });
+      expect(button).toBeEnabled();
     });
   });
 });
