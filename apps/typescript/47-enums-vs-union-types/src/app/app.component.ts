f9fd5e5141d91536e39ab374ada97f744ad2793a
Narcis <narcisrossello@gmail.com>
2024-06-11 21:04:00 +0200

    feat: solve challenge 47

--- apps/typescript/47-enums-vs-union-types/src/app/app.component.ts
+++ apps/typescript/47-enums-vs-union-types/src/app/app.component.ts
@@ -1,27 +1,45 @@
+import { TitleCasePipe } from '@angular/common';
 import { Component, computed, signal } from '@angular/core';
 
-enum Difficulty {
+enum DifficultyEnum {
   EASY = 'easy',
   NORMAL = 'normal',
 }
 
-enum Direction {
+type Difficulty = keyof typeof difficulty;
+const difficulty = {
+  easy: DifficultyEnum.EASY,
+  normal: DifficultyEnum.NORMAL,
+};
+
+enum DirectionEnum {
   LEFT = 'left',
   RIGHT = 'right',
 }
 
+type Direction = 'left' | 'right';
+type DirectionMap = { [key in Direction]: string };
+const directions: DirectionMap = {
+  left: 'left',
+  right: 'right',
+};
+
 @Component({
   standalone: true,
-  imports: [],
+  imports: [TitleCasePipe],
   selector: 'app-root',
   template: `
     <section>
       <div>
-        <button mat-stroked-button (click)="difficulty.set(Difficulty.EASY)">
-          Easy
+        <button
+          mat-stroked-button
+          (click)="difficulty.set(DifficultyEnum.EASY)">
+          {{ DifficultyEnum.EASY | titlecase }}
         </button>
-        <button mat-stroked-button (click)="difficulty.set(Difficulty.NORMAL)">
-          Normal
+        <button
+          mat-stroked-button
+          (click)="difficulty.set(DifficultyEnum.NORMAL)">
+          {{ DifficultyEnum.NORMAL | titlecase }}
         </button>
       </div>
       <p>Selected Difficulty: {{ difficultyLabel() }}</p>
@@ -29,10 +47,10 @@ enum Direction {
 
     <section>
       <div>
-        <button mat-stroked-button (click)="direction.set(Direction.LEFT)">
+        <button mat-stroked-button (click)="direction.set(DirectionEnum.LEFT)">
           Left
         </button>
-        <button mat-stroked-button (click)="direction.set(Direction.RIGHT)">
+        <button mat-stroked-button (click)="direction.set(DirectionEnum.RIGHT)">
           Right
         </button>
       </div>
@@ -54,30 +72,20 @@ enum Direction {
   `,
 })
 export class AppComponent {
-  readonly Difficulty = Difficulty;
-  readonly difficulty = signal<Difficulty>(Difficulty.EASY);
+  readonly DifficultyEnum = DifficultyEnum;
+  readonly DirectionEnum = DirectionEnum;
 
-  readonly Direction = Direction;
+  readonly difficulty = signal<Difficulty>(DifficultyEnum.EASY);
+
   readonly direction = signal<Direction | undefined>(undefined);
 
-  readonly difficultyLabel = computed<string>(() => {
-    switch (this.difficulty()) {
-      case Difficulty.EASY:
-        return Difficulty.EASY;
-      case Difficulty.NORMAL:
-        return Difficulty.NORMAL;
-    }
-  });
+  readonly difficultyLabel = computed<string>(
+    () => difficulty[this.difficulty()],
+  );
 
-  readonly directionLabel = computed<string>(() => {
-    const prefix = 'You chose to go';
-    switch (this.direction()) {
-      case Direction.LEFT:
-        return `${prefix} ${Direction.LEFT}`;
-      case Direction.RIGHT:
-        return `${prefix} ${Direction.RIGHT}`;
-      default:
-        return 'Choose a direction!';
-    }
-  });
+  readonly directionLabel = computed<string>(() =>
+    this.direction()
+      ? `You chose to go ${directions[this.direction()!]}`
+      : 'Choose a direction!',
+  );
 }
